fe57f42e9ba955c5063d5961e0b03a774fb3328c
Aditya Chandra <inibukanadit@gmail.com>
2020-11-20 23:01:57 +0700

    init template

---
+++ app/src/main/java/com/addeeandra/akm/template/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.addeeandra.akm.template
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
